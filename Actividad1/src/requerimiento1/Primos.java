95a24d0f8a109ea4dd5a7c7f5af5d4395aa8f7f4
Sergio2600 <77533955+Sergio2600@users.noreply.github.com>
2021-10-11 13:10:52 +0200

    meto el codigo en la clase Primos

--- Actividad1/src/requerimiento1/Primos.java
+++ Actividad1/src/requerimiento1/Primos.java
@@ -1,5 +1,53 @@
 package requerimiento1;
 
-public class Primos {
-khkhk
+public class Primos implements Runnable{
+
+	public int numero;
+
+	
+	public Primos() {
+		super();
+	}
+
+	public Primos(int numero) {
+		super();
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		
+		this.numero = numero;
+	}
+	
+	// Para saber si un número es primo debe ser divisible solo 
+	// por si mismo y por 1
+	
+	@Override
+	public void run() {
+		int contador = 0;
+		int numDivisiblesPrimo = 2;
+		long inicio = System.currentTimeMillis();
+		
+		for(int i = 1; i <= numero; i++) {
+			if(numero % i == 0) {
+				contador++;				
+			}			
+		}
+		
+		if(contador == numDivisiblesPrimo) {
+			System.out.println("El número " + this.numero + " es primo");
+		} else {
+			System.out.println("El número " + this.numero + " no es primo");
+		}
+		
+		long final = System.currentTimeMillis();
+		int tiempo = (int) ((fin - inicio));
+		System.out.println("Hilo: " + Thread.currentThread().getName() + " acabado y ha tardado: " + tiempo + " milisegundos");
+
+	}
+
 }
