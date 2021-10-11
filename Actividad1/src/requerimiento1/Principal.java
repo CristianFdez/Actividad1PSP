f08e72cfdf63f93574f077f2b32593353a0923d9
Sergio2600 <77533955+Sergio2600@users.noreply.github.com>
2021-10-11 13:06:20 +0200

    meto el codigo en la clase Principal

--- Actividad1/src/requerimiento1/Principal.java
+++ Actividad1/src/requerimiento1/Principal.java
@@ -1,5 +1,40 @@
 package requerimiento1;
 
+import java.util.Scanner;
+
 public class Principal {
+
+	public static void main(String[] args) {
+		Primos primo1 = new Primos();
+		Thread t1 = new Thread(primo1);
+		t1.setName("Primo1");
+		
+		Primos primo2 = new Primos();
+		Thread t2 = new Thread(primo2);
+		t2.setName("Primo2");
+		
+		Primos primo3 = new Primos();
+		Thread t3 = new Thread(primo3);
+		t3.setName("Primo3");
+		
+		Primos primo4 = new Primos();
+		Thread t4 = new Thread(primo4);
+		t4.setName("Primo4");
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Introduce 4 números:");
+		primo1.setNumero(sc.nextInt());		
+		primo2.setNumero(sc.nextInt());
+		primo3.setNumero(sc.nextInt());
+		primo4.setNumero(sc.nextInt());
+		sc.close();
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+	}
+
+
 
 }
