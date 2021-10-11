94f842831d9552dff9cc5259d385231934602911
CristianFdez <cristian.titan95@gmail.com>
2021-10-11 12:46:54 +0200

    Modificación de la clase main Principal.java

--- Actividad1/src/requerimiento1/Principal.java
+++ Actividad1/src/requerimiento1/Principal.java
@@ -1,33 +1,40 @@
-package actividad1;
+package requerimiento1;
 
 import java.util.Scanner;
 
-public class Principal implements Runnable{
+public class Principal {
 
 	public static void main(String[] args) {
 		Primos primo1 = new Primos();
+		Thread t1 = new Thread(primo1);
+		t1.setName("Primo1");
+		
 		Primos primo2 = new Primos();
+		Thread t2 = new Thread(primo2);
+		t2.setName("Primo2");
+		
 		Primos primo3 = new Primos();
+		Thread t3 = new Thread(primo3);
+		t3.setName("Primo3");
+		
 		Primos primo4 = new Primos();
+		Thread t4 = new Thread(primo4);
+		t4.setName("Primo4");
 		
 		Scanner sc = new Scanner (System.in);
-		primo1.setNumero(sc.nextInt());
+		System.out.println("Introduce 4 números:");
+		primo1.setNumero(sc.nextInt());		
 		primo2.setNumero(sc.nextInt());
 		primo3.setNumero(sc.nextInt());
 		primo4.setNumero(sc.nextInt());
 		sc.close();
 		
-		System.out.println("El número " + primo1.getNumero() +" es primo: " + primo1.esPrimo());
-		System.out.println("El número " + primo2.getNumero() +" es primo: " + primo2.esPrimo());
-		System.out.println("El número " + primo3.getNumero() +" es primo: " + primo3.esPrimo());
-		System.out.println("El número " + primo4.getNumero() +" es primo: " + primo4.esPrimo());
-
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 }
