324fd7fc9c8f2bbaeb63d729aa1c18467b1ff6eb
CristianFdez <cristian.titan95@gmail.com>
2021-10-12 12:31:16 +0200

    Actualizo la clase Main.java

--- Actividad1/src/requerimiento2/Main.java
+++ Actividad1/src/requerimiento2/Main.java
@@ -5,9 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Cola cola = new Cola();
 		
-		Productor p1 = new Productor();
+		//Productor p1 = new Productor("Producto 1",cola);
 
 		
+		//Consumidor c1 = new Consumidor("Consumidor 1",cola);
+
+
 	}
 
 }
